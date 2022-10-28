f0a3f9343c688880d0cf072b6106d4dda5290c9f
Jacky Jiang <t83714@gmail.com>
2022-10-29 00:33:24 +1100

    fixed: spatial-coverage should be set bbox as numbers rather strings

--- aspect-templates/spatial-coverage.js
+++ aspect-templates/spatial-coverage.js
@@ -1,13 +1,26 @@
-if(dataset?.spatialParameters?.projection !== "WGS84"){
+if (dataset?.spatialParameters?.projection !== "WGS84") {
     return {};
 } else {
+    const westLongitude = dataset?.spatialParameters?.westLongitude;
+    const southLatitude = dataset?.spatialParameters?.southLatitude;
+    const eastLongitude = dataset?.spatialParameters?.eastLongitude;
+    const northLatitude = dataset?.spatialParameters?.northLatitude;
+
+    const bboxItems = [
+        westLongitude,
+        southLatitude,
+        eastLongitude,
+        northLatitude
+    ]
+        .map((item) => parseFloat(item))
+        .filter((item) => !isNaN(item));
+
+    if (bboxItems.length !== 4) {
+        return {};
+    }
+    
     return {
-        "spatialDataInputMethod": "bbox",
-        "bbox": [
-            dataset?.spatialParameters?.westLongitude,
-            dataset?.spatialParameters?.southLatitude,
-            dataset?.spatialParameters?.eastLongitude,
-            dataset?.spatialParameters?.northLatitude
-        ]
+        spatialDataInputMethod: "bbox",
+        bbox: bboxItems
     };
 }
